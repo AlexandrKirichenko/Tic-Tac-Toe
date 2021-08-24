bb77b8a2d942312271f1fdcec61052dcca710d46
VladimirDiamandi <vladdiamandi@gmail.com>
2021-08-24 17:33:10 +0300

    refactor in component Game add arrow function getApponent

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -8,7 +8,7 @@ import Move from '../Move/Move'
 
 const Game = () => {
     const [board, setBoard] = useState(Array(9).fill(null))
-    const [X, setX] = useState(true);
+    const [player, setPlayer] = useState('X');
     const [isDraw, setIsDraw] = useState (false);
     const [winner, setWinner] = useState(null);
     
@@ -25,26 +25,30 @@ const Game = () => {
         
     }, [board]);
     
+    const getApponent = (player) => {
+        return player == 'X' ? 'O' : 'X';
+    }
+    
     const handleClick = (index) => {
  
         const boardCopy = [...board]
         if (winner || boardCopy[index]) return
-        
-        boardCopy[index] = X ? 'X' : 'O'
+        console.log(getApponent('X'))
+        boardCopy[index]=getApponent(player);
         setBoard(boardCopy)
-        setX(!X)
+        setPlayer(getApponent(player))
     }
     
     const btnClk = () => {
         setBoard(Array(9).fill(null));
-        setX(true);
+        setPlayer('X');
         setWinner(null);
         setIsDraw(false);
     }
     const disableColor = Boolean(winner || isDraw)
     return (
         <div className="wrapper">
-            <Move player={X} disableColor={disableColor}/>
+            <Move player={player} disableColor={disableColor}/>
             <Board squares={board} click={handleClick} disableColor={disableColor}/>
             {winner || isDraw ? <Winner winner={winner}  isDraw={isDraw} btnClk={btnClk}/> : null}
         </div>
